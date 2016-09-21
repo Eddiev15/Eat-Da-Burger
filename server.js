1dbb0a8191d27a74335cdec6e31ba9a7f92cfed5
Michelle Hettinger <Michelle Hettinger>
2016-09-21 17:52:25 -0400

    changed some text

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-/*
-Here is where you set up your server file.
-express middleware.
-*/
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
 
 var app = express();
+var PORT = process.env.PORT || 3002;
 
 //Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + '/public'));
@@ -16,8 +12,10 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }))
 // override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
+
 var exphbs = require('express-handlebars');
+
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
@@ -26,5 +24,4 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var PORT = process.env.PORT || 3002;
 app.listen(PORT);
