0b066184532feab0c220331be3640a1929cd62ec
Michelle Hettinger <Michelle Hettinger>
2016-09-21 18:49:21 -0400

    modified package.json

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
+var exphbs = require('express-handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
 
 var app = express();
 var PORT = process.env.PORT || 3002;
@@ -11,17 +14,17 @@ app.use(express.static(process.cwd() + '/public'));
 app.use(bodyParser.urlencoded({
 	extended: false
 }))
+
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
-var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
-var routes = require('./controllers/burgers_controller.js');
+
 app.use('/', routes);
 
 app.listen(PORT);
